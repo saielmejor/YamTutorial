3d003057204674307716880bbc3ab07b276c795c
SAIKEN <SAIKEN@DESKTOP-HQRQLHN>
2024-03-08 20:56:48 -0500

    added mainnav

---
+++ Frontend/vite-project/src/components/MainNav.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+export default function MainNav() {
+  return (
+    <Button variant="ghost" className='font-bold hover:text-orange-500 hover:bg-white'> 
+
+    </Button>
+  )
+}
